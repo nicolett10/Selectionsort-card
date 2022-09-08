fbd47c7526203fa3f308c2ca8e09654c4c2e6e27
nicolett10 <torrestapia.n@gmail.com>
2022-09-08 00:50:19 +0000

    selectioncard

--- src/app.js
+++ src/app.js
@@ -1,11 +1,103 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+// variables
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let Suit = null;
+  let Number = null;
+  let Switchnumb = null;
+  let Cards = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Generar type suit
+  let generaterandomtype = () => {
+    let type = ["diamond", "spade", "heart", "club"];
+    let indextype = Math.floor(Math.random() * type.length);
+    return type[indextype];
+  };
+  // generar  number
+  let generaterandomnumber = () => {
+    let Numbers = Math.floor(Math.random() * 13) + 1;
+    return Numbers;
+  };
+  function switchvalue(valor) {
+    switch (valor) {
+      case 1:
+        return "A";
+      case 2:
+        return "2";
+      case 3:
+        return "3";
+      case 4:
+        return "4";
+      case 5:
+        return "5";
+      case 6:
+        return "6";
+      case 7:
+        return "7";
+      case 8:
+        return "8";
+      case 9:
+        return "9";
+      case 10:
+        return "10";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        return valor;
+    }
+  }
+
+  // boton evento draw
+  let button = document.getElementById("Draw");
+  button.addEventListener("click", function() {
+    let inputnumber = document.getElementById("inputnumber").value;
+
+    for (let i = 0; i < inputnumber; i++) {
+      Number = generaterandomnumber();
+      Suit = generaterandomtype();
+      Switchnumb = switchvalue(Number);
+      Cards.push({ numeros: Number, type: Suit });
+
+      let newElement = document.createElement("div");
+      newElement.classList.add("card");
+      newElement.classList.add(Suit);
+      newElement.textContent = switchvalue(Number);
+      document.querySelector(".table").appendChild(newElement);
+    }
+  });
+
+  console.log(Cards);
+
+  // selection sort
+  let Selectioncard = document.getElementById("Selection");
+  Selectioncard.addEventListener("click", function() {
+    let size = Cards.length;
+    for (let i = 0; i < size; i++) {
+      let min = i;
+      for (let j = min; j < size; j++) {
+        if (Cards[min].numeros > Cards[j].numeros) {
+          let temp = Cards[min];
+          Cards[min] = Cards[j];
+          Cards[j] = temp;
+        }
+      }
+
+      let newdiv = document.createElement("div");
+      newdiv.classList.add("fila");
+      for (let n = 0; n < Cards.length; n++) {
+        let icon = switchvalue(Cards[n].numeros);
+        let newElement2 = document.createElement("div");
+        newElement2.classList.add("card");
+        newElement2.classList.add(Cards[n].type);
+        newElement2.textContent = icon;
+        newdiv.appendChild(newElement2);
+      }
+      document.querySelector(".Optionrandom").appendChild(newdiv);
+    }
+    console.log(Cards);
+  });
 };
